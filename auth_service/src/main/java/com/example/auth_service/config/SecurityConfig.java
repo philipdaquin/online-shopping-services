fc849471756606855fa3bfe1161f7f158940566b
philipdaquin <philipdaquin20@gmail.com>
2023-08-14 16:56:47 +0800

    user details and authentication provider

--- auth_service/src/main/java/com/example/auth_service/config/SecurityConfig.java
+++ auth_service/src/main/java/com/example/auth_service/config/SecurityConfig.java
@@ -3,17 +3,22 @@ package com.example.auth_service.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.example.auth_service.security.DomainUserService;
 import com.example.auth_service.security.jwt.JWTAuthenticationFilter;
+import com.example.auth_service.service.AccountService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,6 +29,9 @@ import lombok.RequiredArgsConstructor;
 public class SecurityConfig  {
     
     private final JWTAuthenticationFilter jwtAuthenticationFilter;
+
+    private DomainUserService domainUserService;
+    
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { 
@@ -53,8 +61,9 @@ public class SecurityConfig  {
                 .logoutUrl("/logout")
                 .getLogoutSuccessHandler()
             )   
-            
+
             .httpBasic(basic -> basic.init(http))
+            .authenticationProvider(authenticationProvider())
 
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 
@@ -71,4 +80,13 @@ public class SecurityConfig  {
         return config.getAuthenticationManager();
     }
 
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() { 
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(domainUserService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
+ 
 }
---
+++ auth_service/src/main/java/com/example/auth_service/security/DomainUserService.java
@@ -0,0 +1,66 @@
+package com.example.auth_service.security;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.auth_service.domain.actors.Account;
+import com.example.auth_service.errors.NotFoundException;
+import com.example.auth_service.repository.AccountRepository;
+
+/**
+ *  Authentication Provider 
+ *  Validate user crendentials and generate an authenticated 
+ *  pricipal iser object. 
+ *  
+ * 
+ * 
+ */
+@Component("userDetailsService")
+public class DomainUserService implements UserDetailsService {
+
+    private final Logger log = LoggerFactory.getLogger(DomainUserService.class);
+
+    private final AccountRepository accountRepository;
+
+
+    public DomainUserService(
+        AccountRepository accountRepository
+    ) { 
+        this.accountRepository = accountRepository;
+    }
+
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        if (email == null) throw new UsernameNotFoundException("Missing user:" + email);
+        
+        Account account = accountRepository
+            .findByEmailIgnoreCase(email)
+            .orElseThrow(() -> new UsernameNotFoundException("Missing User:" + email));
+        
+        return createSecurityUser(email, account);
+    }
+
+    private User createSecurityUser(String email, Account account) { 
+        List<GrantedAuthority> authorities = account    
+            .getAuthorities()
+            .stream()
+            .map(auth -> new SimpleGrantedAuthority(auth.getName()))
+            .collect(Collectors.toList());
+        
+        return new User(email, account.getPassword(), authorities);
+    }
+}
