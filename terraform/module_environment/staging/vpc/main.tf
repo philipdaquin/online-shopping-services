97ee035ba795356ba92f7c084310c3b9bd0a047a
philipdaquin <philipdaquin20@gmail.com>
2023-08-26 11:24:08 +0800

    module environemtn for staging

---
+++ terraform/module_environment/staging/vpc/main.tf
@@ -0,0 +1,32 @@
+
+
+provider "aws" {
+    region = "ap-southeast-2" 
+    access_key = var.ACCESS_KEY
+    secret_key=  var.SECRET_KEY
+}
+
+terraform { 
+    backend "local" { 
+        path = "dev/vpc/terraform.tfstate"
+    }
+}
+
+module "vpc" { 
+
+    source = "../../../modules/vpc"
+
+    env = "staging"
+    azs = ["ap-southeast-2a", "ap-southeast-2b"]
+    private_subnets = ["10.0.0.0/19", "10.0.32.0/19"]
+    public_subnets = ["10.0.64.0/19", "10.0.96.0/19"]
+
+    private_subnet_tags = {
+        "kubernetes.io/role/internal-elb"   = "1"
+        "kubernetes.io/cluster/dev-demo"    = "owned"
+    }
+    public_subnet_tags = {
+        "kubernetes.io/role/elb"   = "1"
+        "kubernetes.io/cluster/dev-demo"    = "owned"
+    }
+}
