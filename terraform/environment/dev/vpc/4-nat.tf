53b1d62576c1753a94f9a6a89d6b465865ce3a6e
philipdaquin <philipdaquin20@gmail.com>
2023-08-26 11:23:08 +0800

    dev sample terraform

---
+++ terraform/environment/dev/vpc/4-nat.tf
@@ -0,0 +1,22 @@
+/*
+    Network Address Translation Gateway
+    - NAT helps manage traffic flows for private resources that need to communicate with external servces 
+*/
+resource aws_eip nat { 
+    vpc = true
+
+    tags = { 
+        Name = "dev-nat"
+    }
+}
+
+resource aws_nat_gateway nat { 
+    allocation_id = aws_eip.nat.id
+    
+    subnet_id = aws_subnet.public_southeast_2a.id
+    tags = { 
+        Name = "dev-nat"
+    }
+
+    depends_on = [aws_internet_gateway.gateway]
+}
